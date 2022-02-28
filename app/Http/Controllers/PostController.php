b967c0a5cf3a28ca5371ee53fb22954cd43082e3
hiroto0801 <rohito.0423-tkh@outlook.jp>
2022-02-28 20:04:37 +0900

    8-4途中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,14 +6,19 @@ use Illuminate\Http\Request;
 use App\Post;
 class PostController extends Controller
 {
-     /**
-     * Post一覧を表示する
-     * 
-     * @param Post Postモデル
-     * @return array Postモデルリスト
-     */
-    public function index(Post $post)
+       
+       public function index(Post $post)
     {
-        return $post->get();
-    }    //
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    } 
+        
+       public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+    return view('posts/create');
+    }   
 }
+
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
-
+Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/create', 'PostController@create');
 
 ?>
